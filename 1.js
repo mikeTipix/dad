9d98ac27f3448b409563eebafa21be31521d6c0d
mikeTipix <38519747+mikeTipix@users.noreply.github.com>
2018-05-22 09:36:38 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-
+alert(1);
 var user = this.unsene.unseneView.me.get("username");
 var pwd = this.unsene.unseneView.credential.get("password");
     //$('#header-row').next().children()[3].innerHTML=$('#header-row').next().children()[3].innerHTML.substring(5);
